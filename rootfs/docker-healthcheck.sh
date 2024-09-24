ca48af5ada04071fd9673633eef90f09a878cdf0
Socheat Sok <socheatsok78@gmail.com>
2024-09-24 15:36:47 +0700

    Perform HEALTHCHECK and attempt to reselect source if none available

--- rootfs/docker-healthcheck.sh
+++ rootfs/docker-healthcheck.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# Perform HEALTHCHECK and attempt to reselect source if none available
 if chronyc activity | grep '0 sources online'; then
+  if [ "${AUTORESELECT:-false}" = true ]; then
+    chronyc reselect
+  fi
   exit 1
 fi
 exit 0
